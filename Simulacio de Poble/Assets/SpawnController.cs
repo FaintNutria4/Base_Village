bfbb6991eb0b04768d81e9ee9aa0c54306735927
FaintNutria4 <faintnutria4@gmail.com>
2024-05-18 03:36:25 +0200

    spawners village commit

---
+++ Simulacio de Poble/Assets/SpawnController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+
+    public enum SpawnMode
+    {
+        Timer,
+        Trigger
+    }
+
+    public SpawnMode spawnMode;
+    public GameObject prefab;
+
+    public float timeUntilSpawn;
+    private float timer;
+    public float unitsPerSpawn = 1;
+
+    public void Spawn()
+    {
+        for (int i = 0; i < unitsPerSpawn; i++) Instantiate(prefab, transform.position, transform.rotation);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time + timeUntilSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpawnMode.Timer == spawnMode && timer >= Time.time)
+        {
+            Spawn();
+            timer = Time.time + timeUntilSpawn;
+        }
+    }
+}
